28f0375a695c7117adc69e88fe86a7c300505dd0
saikatgupta <saikat.gupta1975@gmail.com>
2015-05-28 21:53:15 +0530

    Created new messenger application using JAX-RS
    Signed-off-by: saikatgupta <saikat.gupta1975@gmail.com>

---
+++ messenger/src/main/java/org/koushik/javabrains/messenger/database/DatabaseClass.java
@@ -0,0 +1,24 @@
+package org.koushik.javabrains.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.koushik.javabrains.messenger.model.Message;
+import org.koushik.javabrains.messenger.model.Profile;
+
+public class DatabaseClass {
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+
+	public static Map<Long, Message> getMessages() {
+		if (messages.size() == 0) {
+			messages.put(1L, new Message(1L, "Hello World!!!", "Koushik"));
+			messages.put(2L, new Message(2L, "Hello Jersey!!!", "Saikat"));
+		}
+		return messages;
+	}
+
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	}
+}
